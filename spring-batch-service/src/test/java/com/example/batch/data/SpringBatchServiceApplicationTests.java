9436e998db3779e4984e84a93371c3f10d74eb5c
Tenzin Dawa <tnzngdw@gmail.com>
2025-06-08 20:13:07 -0400

    Add files via upload

---
+++ spring-batch-service/src/test/java/com/example/batch/data/SpringBatchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batch.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
